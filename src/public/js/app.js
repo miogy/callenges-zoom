f0f5f9d479fc559b68307285d0f45a0d32ba6165
miogy <jjangrl87@gmail.com>
2023-03-22 23:23:49 +0900

    [add] chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,40 @@
-alert("hi");
+const msgList = document.querySelector("ul");
+const msgForm = document.querySelector("form");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+//메세지 받기
+socket.addEventListener("open", () => {
+  console.log("connected to browser 🫠");
+});
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  msgList.append(li);
+  console.log("new message:", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Dissconnected from server 😈");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser");
+}, 10000);
+
+const hendleSubmit = (e) => {
+  e.preventDefault();
+  const input = msgForm.querySelector("input");
+  socket.send(input.value);
+  console.log(input.value);
+  input.value = "";
+};
+
+const handleNickSubmit = (e) => {
+  e.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(input.value);
+};
+msgForm.addEventListener("submit", hendleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -13,10 +13,25 @@ app.get("/*", (req, res) => res.redirect("/"));
 // const handleListen = () => console.log(`Listening on http://localhost:3000`);
 // app.listen(3000, handleListen);
 
+// 동일한 포트에서 http, ws request 두가지 처리 가능
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-// http와 websocket 동시 사용
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  // console.log(socket);
+  sockets.push(socket);
+  console.log("connected to browser 🫠");
+  socket.on("close", () => console.log("Dissconnected from server 😈"));
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message));
+    console.log(message);
+    // socket.send(message);
+  });
+  socket.send("helloooooo!!"); //메세지 보내기
+});
 
 server.listen(3000, handleListen);
